98e50cd483044162cd9ec7dabd54f08bfce323e3
marcin38 <marcin3880@gmail.com>
2015-09-10 18:27:42 +0200

    Stworzenie tabel Avatars, Users.

---
+++ baza1.sql
@@ -0,0 +1,25 @@
+IF OBJECT_ID('Avatars','U') IS NOT NULL
+	DROP TABLE AVATARS;
+IF OBJECT_ID('USERS', 'U') IS NOT NULL
+	drop table USERS;
+
+----------------------------------------------------
+
+CREATE TABLE [dbo].[Avatars](
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[Image] [image] NOT NULL
+);
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
+	[Name] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](50) NOT NULL,
+	[Hash] [varbinary](50) NOT NULL,
+	[RegistrationDate] [datetime] NOT NULL,
+	[RemovalDate] [datetime] NULL,
+	[Location] [nvarchar](50) NULL,
+	[AvatarId] [int] NOT NULL
+
+	CONSTRAINT [FK_AvatarId_Avatars] FOREIGN KEY ([AvatarId]) REFERENCES [Avatars]([Id])
+);
+
