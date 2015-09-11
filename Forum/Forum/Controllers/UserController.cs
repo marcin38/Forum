390827f8efc37e6fd056d1d5e9a4ed7ed93dca6f
marcin38 <marcin3880@gmail.com>
2015-09-11 23:06:02 +0200

    Stworzenie projektu. Stworzenie zalążka rejestracji.

---
+++ Forum/Forum/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum.Models;
+
+namespace Forum.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ Forum/Forum/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace Forum
+{
+    public partial class Startup
+    {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    ConfigureAuth(app);
+        //}
+    }
+}
