9af15c57ee116d1483e5ef86da45f8341f975d6c
marcin38 <marcin3880@gmail.com>
2015-09-13 23:46:40 +0200

    Dodanie formularza logowania oraz akcji w kontrolerze.

---
+++ Forum/Forum/Controllers/Interfaces/IUserController.cs
@@ -0,0 +1,18 @@
+﻿using Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Forum.Controllers.Interfaces
+{
+    interface IUserController
+    {
+        ActionResult RegisterUser();
+        ActionResult RegisterUser(User user);
+        ActionResult Login();
+        ActionResult Login(AccountCredentials credentials);
+    }
+}
--- Forum/Forum/Controllers/UserController.cs
+++ Forum/Forum/Controllers/UserController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Forum.Models;
+using System.Security.Cryptography;
+using System.Text;
+using Forum.Controllers.Interfaces;
+using System.IO;
 
 namespace Forum.Controllers
 {
-    public class UserController : Controller
+    public class UserController : Controller, IUserController
     {
-        private IForumDBContext db = new ForumDBContext();
+        private IForumDBContext db;
+
+        public UserController()
+        {
+            db = new ForumDBContext();
+        }
 
         public UserController(IForumDBContext db)
         {
@@ -33,14 +42,59 @@ namespace Forum.Controllers
             if (ModelState.IsValid)
             {
                 db.Users.Add(user);
-                user.Hash = System.Text.Encoding.UTF8.GetBytes(user.Password);
+                user.Hash = ComputeHash(user.Password, user.Name);
                 user.RegistrationDate = System.DateTime.Now;
                 db.SaveChanges();
+                return RedirectToAction("Login");
             }
 
-            
-
             return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountCredentials credentials)
+        {
+            byte[] password = ComputeHash(credentials.Password, credentials.Name);
+
+            if (ModelState.IsValid)
+            {
+                List<User> users = db.Users
+                    .Where(u => u.Name == credentials.Name).ToList();
+
+                if (users.Count == 0)
+                {
+                    ModelState.AddModelError("", "There is no user with that name");
+                }
+                else if (!password.SequenceEqual(users.ElementAt(0).Hash))
+                {
+                    ModelState.AddModelError("", "Password is wrong");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(credentials);
+        }
+
+        private byte[] ComputeHash(string data, string salt)
+        {
+            
+            //RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            //StreamReader reader = new StreamReader(@"D:\git\Forum\PublicOnlyKey.xml");
+            //string publicOnlyKeyXML = reader.ReadToEnd();
+            //rsa.FromXmlString(publicOnlyKeyXML);
+            //reader.Close();
+            
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] hash = sha.ComputeHash(Encoding.ASCII.GetBytes(String.Format("{0}{1}",data,salt)));
+            return hash;
         }
     }
 }
