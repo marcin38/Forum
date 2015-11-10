eb449a0037439cec77589d6f9d415e91e25b6707
marcin38 <marcin3880@gmail.com>
2015-11-10 17:45:40 +0100

    Zmiany pozwalające na odinstalowanie EF z głównego projektu oraz testów (usunięcie problemu z wstrzykiwania zależności).

--- Forum/Repositories/Repositories/UserRepository.cs
+++ Forum/Repositories/Repositories/UserRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Domain.Models;
 using Repositories.Repositories.Interfaces;
+using System;
+using System.Data.SqlClient;
 
 namespace Repositories.Repositories
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
-        public UserRepository(ForumDBContext context) : base(context){ }
+        public UserRepository(ForumDBContext context) : base(context){ }        
     }
 }
